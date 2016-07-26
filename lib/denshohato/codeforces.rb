3306325e3c72b1a69799a697bc71ba41d7881caa
Bryce Tsao <tsaodingtw@gmail.com>
2016-07-26 10:12:54 +0800

    Implement Codeforces submitter

--- lib/denshohato/codeforces.rb
+++ lib/denshohato/codeforces.rb
@@ -1,8 +1,23 @@
 require "mechanize"
+require "time"
 
 module CodeForces
   def self.submit prob, lang, code
-    
+    agent = Mechanize.new
+    #log in
+    agent.get "http://codeforces.com/enter"
+    form = agent.page.form
+    form.handle = "WLSH"
+    form.password = ""
+    form.submit
+    #submit
+    agent.get "http://codeforces.com/problemset/submit"
+    form.submittedProblemCode = prob
+    form.programTypeId = lang
+    form.source = "// " + Time.now.to_s + "\n" + code
+    form.submit
+    #get result
+    #log out
+    agent.page.link_with("text" => "Logout").click
   end
 end
-
